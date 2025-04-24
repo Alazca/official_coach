3bff6cfb69b6f1f2d8c72b25d674d9cb8a5b1bc3
Alazca <andylazcanop@gmail.com>
2025-04-24 01:20:06 -0700

    added login page

---
+++ frontend/js/credentials.js
@@ -0,0 +1,30 @@
+/**
+ * credentials.js
+ * Manages JWT tokens and authentication status
+ */
+
+const Credentials = (function () {
+    const TOKEN_KEY = 'jwt_token';
+
+    return {
+        // Save JWT to localStorage
+        saveToken: function (token) {
+            localStorage.setItem(TOKEN_KEY, token);
+        },
+
+        // Retrieve JWT from localStorage
+        getToken: function () {
+            return localStorage.getItem(TOKEN_KEY);
+        },
+
+        // Remove JWT (on logout)
+        clearToken: function () {
+            localStorage.removeItem(TOKEN_KEY);
+        },
+
+        // Check if user is logged in
+        isAuthenticated: function () {
+            return !!localStorage.getItem(TOKEN_KEY);
+        }
+    };
+})();
